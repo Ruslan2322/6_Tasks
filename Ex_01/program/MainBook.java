22c8845782d1396c516074615beaf581f9bede76
Ruslan2322 <62638170+Ruslan2322@users.noreply.github.com>
2020-06-23 15:22:57 +0300

    Add files via upload

---
+++ Ex_01/program/MainBook.java
@@ -0,0 +1,10 @@
+package Lesson_06.Ex_01;
+
+import Lesson_06.Ex_01.User.BooksAccount;
+
+public class MainBook {
+    public static void main(String[] args) {
+        BooksAccount booksAccount=new BooksAccount();
+        booksAccount.menu();
+    }
+}
